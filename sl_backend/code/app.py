5bebdf88642dab417347e4cc6d6e914241123264
KatieFizzy <hellokatiefizzy@gmail.com>
2019-07-02 10:03:44 -0400

    port setup

--- sl_backend/code/app.py
+++ sl_backend/code/app.py
@@ -41,5 +41,7 @@ api.add_resource(UserList, '/users')
 if __name__ == '__main__':
     from db import db #imports at bottom of code, because of circular imports
     db.init_app(app)
-    app.run(port=port, debug=True)
+    #app.run(port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
