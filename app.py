ee2ab4a72a896c6fcde5bfaf40964bae1ee61289
KatieFizzy <hellokatiefizzy@gmail.com>
2019-07-10 15:31:28 -0400

    added cors

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-#from flask_jwt import JWT
+from flask_jwt import JWT
+from flask_cors import CORS
 
-#from security import authenticate, identity
+from security import authenticate, identity
 
 from resources.user_resource import UserRegister, UserList, User
 from resources.love_note_resource import LoveNote, LoveNoteList
@@ -13,11 +14,12 @@ from resources.love_note_resource import LoveNote, LoveNoteList
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #DATABSE_URL defined in heroku, locally use sqlite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-#app.secret_key = 'something' #TODO UPDATE ME
+app.secret_key = 'something' #TODO UPDATE ME
 api = Api(app)
 
 
@@ -26,7 +28,7 @@ api = Api(app)
     #db.create_all()
 
 
-#jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWT(app, authenticate, identity)  # /auth
 #authenticate is a function, the returned user becomes identity
 
 
