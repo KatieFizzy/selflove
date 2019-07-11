36cd1775d568b44215bf2ab4f0d837b15a55f6ed
KatieFizzy <hellokatiefizzy@gmail.com>
2019-07-11 17:07:34 -0400

    ugh

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 
-from app import APP
+from app import app
 from db import db
 
-db.init_app(APP)
+db.init_app(app)
 
-@APP.before_first_request
+@app.before_first_request
 def create_tables():
     db.create_all()
